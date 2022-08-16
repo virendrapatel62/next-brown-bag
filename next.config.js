90b0950ad2b848a3dc1895c7931fc5e4eec76cd9
Virendra Kumar Patel <Virendra.Patel@dowjones.com>
2022-08-16 16:10:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    deviceSizes: [200, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [200, 640, 750, 828, 1080, 1200, 1920],
   },
 };
 
